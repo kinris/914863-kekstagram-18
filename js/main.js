27192dc6a3c1b537b88c35690dcc87a6e6174216
Kinris <kinris@yandex.ru>
2019-09-23 22:17:06 +0300

    1.Создание массива из 25 элементов

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
 function getRandomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
-
-var RANDOM_PHOTO = getRandomInteger(1, 25);
-var URL_PHOTO = 'photos/' + RANDOM_PHOTO + '.jpg';
 var DESCRIPTION = [
   'Отличная фотография',
   'Хорошая фотография',
   'Плохая фотография',
 ];
-var LIKES = getRandomInteger(15, 200);
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -21,7 +17,7 @@ var COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-var  USERNAMES = [
+var USERNAMES = [
   'Дина',
   'Берта',
   'Фёдор',
@@ -34,4 +30,32 @@ var  USERNAMES = [
   'Ольга',
   'Лев',
 ];
+var NUMBER_PICTURES = 25;
 
+var getPictures = function () {
+  var picturesList = [];
+  for (var i = 1; i <= NUMBER_PICTURES; i++) {
+    var comments = [];
+    var countComments = getRandomInteger(1, 7);
+    for (var j = 0; j < countComments; j++) {
+      var comment = {
+        avatar: 'img/avatar' + getRandomInteger(1, 6) + '.svg',
+        message: COMMENTS[getRandomInteger(0, COMMENTS.length - 1)],
+        name: USERNAMES[getRandomInteger(0, USERNAMES.length - 1)],
+      };
+      comments.push(comment);
+    };
+    var pictures = {
+      url: 'photos/' + i + '.jpg',
+      description: DESCRIPTION[getRandomInteger(0, DESCRIPTION.length - 1)],
+      likes: getRandomInteger(15, 200),
+      comments: comments,
+    };
+    picturesList.push(pictures);
+  };
+  return picturesList;
+};
+
+var pictures = getPictures();
+console.log(pictures);
+//---------------------------
